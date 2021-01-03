1e00cd9b3d97689bf5935f3be563af064708272a
Andrew Smith <andrewcs149@gmail.com>
2021-01-02 17:43:39 -0800

    Add comments.

--- acp.ps1
+++ acp.ps1
@@ -1,9 +1,15 @@
 # add - commit - push - lazy
 function acp {
+  # prompt for a commit message and the desired branch to push to
   $message = read-host -prompt 'Message'
   $branch = read-host -prompt 'Branch'
 
+  # git add all files
   git add .
+
+  # git commit
   git commit -a -m "$message"
+
+  # git push
   git push origin "$branch"
 }
--- new-console.ps1
+++ new-console.ps1
@@ -1,13 +1,23 @@
 # creates a new dotnet console app and adds a solution to the csproj file
 function new-console {
+  # name: name of console app
   Param(
     [string]$name
   )
 
+  # create a new directory and cd into new directory
   mkdir $name
   cd .\$name
+
+  # create a new dotnet console app
   dotnet new console -n $name
+
+  # create a new solution 
   dotnet new sln
+
+  # add .csproj file to solution
   dotnet sln add .\$name\"$name".csproj
+
+  # cd into project folder
   cd .\$name
 }
